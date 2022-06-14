1651469c660dcd3af5e13829bdb65fe2e192dbb6
yinoob <1243876958@qq.com>
2022-06-14 19:49:41 +0800

    增加搜索功能

---
+++ js/Fuse-6.6.2/docs/.vuepress/nav/en.js
@@ -0,0 +1,32 @@
+module.exports = [
+  { text: 'Guide', link: '/' },
+  // { text: 'Blog', link: '/blog/' },
+  // {
+  //   text: 'Team',
+  //   link: '/team/'
+  // },
+  {
+    text: 'Sponsor',
+    link: '/support/'
+  },
+  {
+    text: 'Twitter',
+    link: 'https://twitter.com/kirorisk'
+  },
+  {
+    text: 'GitHub',
+    link: 'https://github.com/krisk/Fuse'
+  }
+  // {
+  //   text: 'Jobs',
+  //   link: '/jobs/'
+  // },
+  // {
+  //   text: 'Stories',
+  //   link: '/stories/'
+  // },
+  // {
+  //   text: 'Release Notes',
+  //   link: 'https://github.com/krisk/Fuse/releases'
+  // }
+]
